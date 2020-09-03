75572ea8c740de68eb45b35971f4518739f94722
EduTechTainMent <44534503+Waga43@users.noreply.github.com>
2020-09-03 12:44:29 +0100

    Edited

--- telegram_gcloner/handlers/get_help.py
+++ telegram_gcloner/handlers/get_help.py
@@ -18,13 +18,13 @@ def init(dispatcher: Dispatcher):
 
 @restricted
 def get_help(update, context):
-    message = '发送google drive链接，或者转发带有google drive的信息即可手动转存。\n' \
-              '需要使用 /sa 和 /folders 进行配置\n\n' \
-              '以下是本BOT的命令：\n\n' \
-              '/folders - 设置收藏文件夹\n' \
-              '/sa - 仅限私聊，上传包含sa的ZIP文件夹，在标题写上/sa设置Service Account\n' \
-              '/4999baoyue - 仅限私聊，商业洽谈，请附上留言\n' \
-              '/help - 输出本帮助\n'
+    message = 'Send google drive link, or forward the information with google drive to save manually.\n' \
+              'Need to use /sa and /folders for configuration\n\n' \
+              'The following is the command of this BOT：\n\n' \
+              '/folders - Set favorite folder\n' \
+              '/sa - For private chat only, upload the ZIP folder containing sa, and write /sa in the title to set Service Account\n' \
+              '/4999baoyue - Private chat only, business negotiation, please attach a message\n' \
+              '/help - Output this help\n'
     rsp = update.message.reply_text(message)
     rsp.done.wait(timeout=60)
     message_id = rsp.result().message_id
