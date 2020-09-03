69b10d1d2421b7133227019785e792ad3f23821f
EduTechTainMent <44534503+Waga43@users.noreply.github.com>
2020-09-03 12:16:06 +0100

    Update process.py

--- telegram_gcloner/utils/process.py
+++ telegram_gcloner/utils/process.py
@@ -14,9 +14,9 @@ logger = logging.getLogger(__name__)
 
 def leave_chat_from_message(message, context):
     context.bot.send_message(chat_id=message.chat_id,
-                             text='感谢将本机器人加入本群。' + config.AS_STRING.format(context.bot.username),
+                             text='Thanks for adding this robot to this group.' + config.AS_STRING.format(context.bot.username),
                              parse_mode=ParseMode.HTML)
-    context.bot.send_message(chat_id=message.chat_id, text='本群未认证，已退群')
+    context.bot.send_message(chat_id=message.chat_id, text='This group is not certified and has been withdrawn')
     if message.from_user:
         mention_html_from_user = mention_html(message.from_user.id,
                                               message.from_user.full_name.full_name)
